2cfaf407afba922b76f5dd5eb81926e3c48d8701
Alexis Mayer <38039314+AlexisMayer@users.noreply.github.com>
2023-01-19 22:35:31 +0100

    update

--- inst/app.R
+++ inst/app.R
@@ -10,12 +10,15 @@
 #' 
 #' @export
 app = function(id) {
+  
   # Library
   require(tidyverse)
   require(janitor)
   require(purrr)
   require(shiny)
   require(DT)
+  require(shinySwipe)
+  
   # Load last data and unscrored data
   df = file.info(list.files(paste0("data/get/", id, "/"), full.names = T)) %>%
     rownames_to_column("name") %>% 
@@ -26,35 +29,72 @@ app = function(id) {
     filter(!link %in% read_csv(paste0("data/get/", id,"/score/score.csv"))$link) %>% 
     filter(!desc %in% read_csv(paste0("data/get/", id,"/score/score.csv"))$desc) %>% 
     filter(!duplicated(desc)) 
+  
   # Appli
   shinyApp(
+    
     # UI side
-    ui = fluidPage(fluidRow(column(12, h1("Emploi-R App"), hr(), DTOutput("x")))),
+    ui = ui <- fluidPage(
+      
+    # Titre de l'application
+    titlePanel("Emploi-R App"),  
+      
+    # Zone pour swiper les offres d'emploi
+    shinySwipe(id = "swipe", choices = c("Pass", "Like"), swipe = "Pass"),   
+      
+    # Zone pour afficher les offres d'emploi
+    fluidRow(
+        column(12, DTOutput("dataTable"))
+    )
+    ),
+    
     # Server side
-    server = function(input, output, session) {
-      options(DT.options = list(pageLength = 1))
-      # server-side processing
-      output$x = renderDT(df, selection = 'none', editable = "cell")
-      # edit a single cell
-      proxy = dataTableProxy('x')
-      observeEvent(input$x_cell_edit, {
-        info = input$x_cell_edit
-        str(info)  # check what info looks like 
-        df <<- editData(df, info)
-        replaceData(proxy, df, resetPaging = FALSE)  # the above steps can be merged into a single editData() call; see examples below
-      })
+    server <- function(input, output) {
+      
+    # Charger les données
+    df <- read_csv("offres_emploi.csv")
+      
+    # Afficher les données dans le composant DT
+    output$dataTable <- renderDT(df)
+      
+    # Observer l'événement de swipe
+    observeEvent(input$swipe, {
+      
+        # Récupérer l'index de l'offre d'emploi swipée
+        swipeIndex <- input$swipe_index
+      
+        # Récupérer la valeur de swipe (Pass ou Like)
+        swipeValue <- input$swipe_value
+      
+        # Si l'utilisateur a "liké" l'offre d'emploi
+        if (swipeValue == "Like") {
+            # Ajouter une colonne "note" avec la valeur 1
+            df[swipeIndex, "note"] <- 1
+        }
+        # Si l'utilisateur a "passé" l'offre d'emploi
+        if (swipeValue == "Pass") {
+            # Ajouter une colonne "note" avec la valeur 0
+            df[swipeIndex, "note"] <- 0
+        }
+        
+        # Enregistrer les données modifiées
+        write_csv(df, "offres_emploi.csv")
+    })
     }
   )
+  
   # Save unscored df
   read_csv(paste0("data/get/", id,"/unscore/unscore.csv")) %>% 
     bind_rows(filter(df, is.na(note))) %>% 
     filter(!duplicated(link)) %>% 
     filter(!duplicated(desc)) %>% 
     write_csv(paste0("data/unscore/unscore.csv"))
+  
   # Save scored df
   read_csv(paste0("data/get/", id,"/score/score.csv")) %>% 
     bind_rows(filter(df, !is.na(note)) %>% mutate_at(vars(note), as.numeric)) %>% 
     filter(!duplicated(link)) %>% 
     filter(!duplicated(desc)) %>% 
     write_csv(paste0(paste0("data/get/", id,"score/score.csv")))
+  
 }
